4b94cae1c6a46cbd0b1e19a49e6dc264f7416042
espoirbaraka <esbarakabigeba@gmail.com>
2022-06-25 11:02:09 +0200

    Listes

--- list/exemple1.py
+++
@@ -1,6 +0,0 @@
-pays = ['RDC','Rwanda','Burundi','Tanzanie','Ouganda','Kenya']
-x = input("Saisi un pays \n")
-if x in pays:
-    print("{} est sur la liste".format(x))
-else:
-    print("{} n'est pas sur la liste".format(x))
